eae5730f628ce756725f352cc91e49ac3176ab55
derk <derk.tu@gmail.com>
2012-07-25 23:48:14 +0800

    fixed content number

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,27 +1,7 @@
-require 'will_paginate/array'
+#require 'will_paginate/array'
 class TagsController < ApplicationController
-  def index
-    items_per_page = 30
-    tagname = params[:id].present? ? params[:id].downcase : ""
-    @tag = Tag.find(:first, :conditions => ["LOWER(name) like ?", tagname]) || raise(ActiveRecord::RecordNotFound, "Couldn't find tag #{tagname}")
-    @related_tags = Tag.related_tags(@tag.id)
-
-    username = params[:user_id].present? ? params[:user_id].downcase : ""
-    unless username.blank?
-      @user = User.find_with_username!(username)
-      if current_user == @user
-        @posts = @tag.posts.all(:conditions => "taggings.user_id = #{@user.id}").paginate :page => params[:page], :per_page => items_per_page
-      else
-        @posts = @tag.posts.all(:conditions => "taggings.user_id = #{@user.id}").paginate :page => params[:page], :per_page => items_per_page
-      end
-    else
-      @ShowTagAllActive = 1
-      @posts = @tag.posts.all.paginate :page => params[:page], :per_page => items_per_page
-    end
-  end
-
   def show
-    items_per_page = 30
+    items_per_page = 10
     tagname = params[:id].present? ? params[:id].downcase : ""
     @tag = Tag.find(:first, :conditions => ["LOWER(name) like ?", tagname]) || raise(ActiveRecord::RecordNotFound, "Couldn't find tag #{tagname}")
     @related_tags = Tag.related_tags(@tag.id)
@@ -30,13 +10,13 @@ class TagsController < ApplicationController
     unless username.blank?
       @user = User.find_with_username!(username)
       if current_user == @user
-        @posts = @tag.posts.all(:conditions => "taggings.user_id = #{@user.id}").paginate :page => params[:page], :per_page => items_per_page
+        @posts = @tag.posts.paginate :page => params[:page], :per_page => items_per_page, :conditions => "taggings.user_id = #{@user.id}"
       else
-        @posts = @tag.posts.all(:conditions => "taggings.user_id = #{@user.id}").paginate :page => params[:page], :per_page => items_per_page
+        @posts = @tag.posts.paginate :page => params[:page], :per_page => items_per_page
       end
     else
       @ShowTagAllActive = 1
-      @posts = @tag.posts.all.paginate :page => params[:page], :per_page => items_per_page
+      @posts = @tag.posts.paginate :page => params[:page], :per_page => items_per_page
     end
   end
 end
