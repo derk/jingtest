74b141f7b08a1883e7dcfcf1977f2729e1626486
derk <derk.tu@gmail.com>
2012-07-27 07:30:56 +0800

    update follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -7,7 +7,7 @@ class Follow < ActiveRecord::Base
   belongs_to :followable, :polymorphic => true
   belongs_to :follower,   :polymorphic => true
 
-  after_create :init_visit_journal, :if => Proc.new { |f| f.follower.visit_journals.where(:user_id => f.followable_id).empty? }
+  after_create :init_visit_journal
 
   def block!
     self.update_attribute(:blocked, true)
@@ -15,7 +15,11 @@ class Follow < ActiveRecord::Base
 
 private
   def init_visit_journal
-    VisitJournal.create!(:guest_id => self.follower_id, :user_id => self.followable_id, :last_visited_at => 1.year.ago)
+    journal = VisitJournal.where(:guest_id => self.follower_id, :user_id => self.followable_id).first
+    unless journal
+    	VisitJournal.create!(:guest_id => self.follower_id, :user_id => self.followable_id, :last_visited_at => Time.local(2012,1,1))
+    end
+    true
   end
 
 end
